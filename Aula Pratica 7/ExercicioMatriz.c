cd5059975d9f105f38e332347fce00203130d606
PedroTrento <pedrotrento8@gmail.com>
2024-05-06 16:12:46 -0300

    ExercicioMatriz completo/comentado

--- Aula Pratica 7/ExercicioMatriz.c
+++ Aula Pratica 7/ExercicioMatriz.c
@@ -21,14 +21,17 @@
 
 int main(){
 
+    //declaração de matrizes e arranjos
     char modelo[MAX_CARROS][TAM_MODELO];
     int anoFrabrica[MAX_CARROS];
     char placa[MAX_CARROS][TAM_PLACA];
 
+    //declaração de variáveis
     int seguir = 1, num_carros = 0;
 
+    //loop para pegar as informações até digitarem um ano negativo
     while(seguir){
-        printf("Entre o modelo, ano de fabricacao e placa do veiculo %d:", num_carros+1);
+        printf("Entre o modelo, ano de fabricacao e placa do veiculo %d:\n", num_carros+1);
         fgets(modelo[num_carros], TAM_MODELO, stdin);
         //esse if serve para retirar o enter que o fgets pega ao digitar.
         //poderia ter usado o gets que não pega o enter, mas achei melhor continuar usando o fgets para me acosturmar a utilizar a melhor forma de captar uma string
@@ -37,6 +40,7 @@ int main(){
         }
 
         scanf("%d", &anoFrabrica[num_carros]);
+        //getchar para consumir com o /n gerado pelo enter do scanf e não interferir com o fgets
         getchar();
         fgets(placa[num_carros], TAM_PLACA, stdin);
 
@@ -46,29 +50,22 @@ int main(){
             placa[num_carros][strlen(placa[num_carros]) - 1] = '\0';
         }
 
+        //se o ano for negativo sai do loop e tira decrementa 1 da variável da quantidade de carros escritos
         if(anoFrabrica[num_carros] < 0 ){
             seguir = 0;
             num_carros--;
         }
         num_carros++;
     }
-
     printf("Impressao dos veiculos lidos:\n");
     for(int i = 0; i < num_carros ; i++){
         printf("%s, ano %d, %s \n", modelo[i], anoFrabrica[i], placa[i]);
     }
-
-
-
-    printf("Modelos e anos de fabricação dos veiculos com placa de final 7:\n");
+    printf("Modelos e anos de fabricacao dos veiculos com placa de final 7:\n");
     for(int i = 0; i < num_carros ; i++){
         if(placa[i][strlen(placa[i])-1] == '7'){
             printf("%s, ano %d\n", modelo[i], anoFrabrica[i]);
         }
     }
-
-
-
-
     return 0;
 }
